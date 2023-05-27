3ec86b71690bf59a3235650de972456d02a5047a
RedAlex50 <RedAlex50@mail.ru>
2023-05-27 19:58:08 +0300

    styles

---
+++ script.js
@@ -0,0 +1,8 @@
+function openFormMenu() {
+    document.getElementById('btn_openMenuForm').style.display = "none";
+    document.getElementById('menu_form').style.display = "block";
+}
+function closeFormMenu() {
+    document.getElementById('menu_form').style.display = "none";
+    document.getElementById('btn_openMenuForm').style.display = "block";
+}
